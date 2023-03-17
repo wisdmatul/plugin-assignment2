c0986ecfe6a4c032392d0738f9c7167c67089468
Atul <atul.kumar@wisdmlabs.com>
2023-03-17 15:09:41 +0530

    commit for declaring the header comments

---
+++ email-admin.php
@@ -0,0 +1,10 @@
+<?php
+
+/*
+* Plugin Name: email-admin
+* Author: Atul
+* Author URI: https://wpemailadmin.com
+* Description: a mail for all the posts at the end of the day
+* Text Domain: maildesk
+*/
+
