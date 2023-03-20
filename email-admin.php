5d4b719940497e0060d7023bf8eae6a10bafcf47
Atul <atul.kumar@wisdmlabs.com>
2023-03-20 18:21:09 +0530

    adding google speed score

--- email-admin.php
+++ email-admin.php
@@ -43,7 +43,7 @@ function schedule_email()
 {
     if( !wp_next_scheduled( 'schedule_email' ) )
     {
-        wp_schedule_event(time(), 'daily', 'schedule_email');
+        wp_schedule_event(time(), 'every_minute', 'schedule_email');
     }
 }
 add_action('schedule_email', 'send_emaildata');
@@ -71,7 +71,7 @@ function get_emaildata()
             'meta_title' => get_post_meta($post->ID, '_yoast_wpseo_title', true),
             'meta_description' => get_post_meta( $post->ID, '_yoast_wpseo_metadesc', true ),
             'meta_keywords' => get_post_meta($post->ID, '_yoast_wpseo_focuskw', true),
-            //'page_speed' => get_page_speed_score( get_permalink( $post->ID ) ),
+            'page_speed' => get_page_speed_score( get_permalink( $post->ID ) ),
         );
         array_push($data, $post_data);
     }
@@ -92,7 +92,7 @@ function send_emaildata()
         $message .= 'Metta Title:' . $post_data['meta_title'] . "\n";
         $message .= 'Meta Description' . $post_data['meta_description'] . "\n";
         $message .= 'Meta Keywords:' . $post_data['meta_keywords'] . "\n";
-        //$message .= 'Page Speed Score:' . $post_data['page_speed'] . "seconds \n";
+        $message .= 'Page Speed Score:' . $post_data['page_speed'] . "seconds \n";
         $message .= "\n";
     }
     $headers = array(
@@ -103,3 +103,24 @@ function send_emaildata()
     wp_mail($to, $subject, $message, $headers);
 }
 
+//google page speed score
+function get_page_speed_score($url) {
+
+    $api_key = "416ca0ef-63e4-4caa-a047-ead672ecc874"; // your api key
+	$new_url = "http://www.webpagetest.org/runtest.php?url=".$url."&runs=1&f=xml&k=".$api_key; 
+	$run_result = simplexml_load_file($new_url);
+	$test_id = $run_result->data->testId;
+
+    $status_code=100;
+    
+//     while( $status_code != 200){
+//         sleep(10);
+        $xml_result = "http://www.webpagetest.org/xmlResult/".$test_id."/";
+	    $result = simplexml_load_file($xml_result);
+        $status_code = $result->statusCode;
+//         $time = (float) ($result->data->median->firstView->loadTime)/1000;
+//     };
+
+//     return $time;
+    return $status_code;    
+}
